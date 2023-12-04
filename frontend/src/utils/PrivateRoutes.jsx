6302136e1de6410e973a511b7632ef373a7468e9
tzviki <twilhelm@votis.net>
2023-12-04 14:32:08 +0200

    .

--- frontend/src/utils/PrivateRoutes.jsx
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 
+const {REACT_APP_BACKEND_URL} = import.meta.env
 import cookie from "js-cookie";
 
 function PrivateRoute({ children}) {
@@ -10,7 +11,7 @@ function PrivateRoute({ children}) {
   const [role, setRole] = useState(null);
 
   async function checkAuth() {
-    const res = await fetch("http://localhost:5000/api/v1/auth/verify", {
+    const res = await fetch(`${REACT_APP_BACKEND_URL}auth/verify`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",

83af131fb0757a8a2f4d6a643c088e3371f62fad
tzviki <twilhelm@votis.net>
2023-12-24 12:19:31 +0200

    Update Column module CSS and add scroll functionality

--- frontend/src/components/manager/newOrder/Column.jsx
+++ frontend/src/components/manager/newOrder/Column.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import column from "../../../assets/css/manager/newOrder/Column.module.css";
 import { useOrderContext } from "../../../context/orderContext/OrderContext";
 import Row from "./Row";
@@ -14,6 +14,39 @@ function Column({ name, num, products }) {
   const [arrowRight, setArrowRight] = useState(false);
   const [arrowLeft, setArrowLeft] = useState(false);
 
+  const productListRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollWidth,  scrollLeft,  clientWidth,  } = productListRef.current;
+
+      if(Math.abs(scrollLeft) + clientWidth === scrollWidth){
+        setArrowLeft(false);
+        setArrowRight(true);
+      } else if(scrollLeft === 0 && scrollWidth - scrollLeft > clientWidth){
+        setArrowLeft(true);
+        setArrowRight(false);
+      } else {
+        setArrowLeft(true);
+        setArrowRight(true);
+      }
+
+      
+    };  
+      if(productListRef.current){
+
+        productListRef.current.scrollWidth > productListRef.current.clientWidth ? setArrowLeft(true) : setArrowLeft(false);
+        setArrowRight(false);
+        
+
+        const productListElement = productListRef.current;
+        productListElement.addEventListener("scroll", handleScroll);
+        
+        return () => {
+          productListElement.removeEventListener("scroll", handleScroll);
+        };
+      }
+  }, [open, state.activeDepartment]);
 
   useEffect(() => {
     setOpen(false);
@@ -35,7 +68,7 @@ function Column({ name, num, products }) {
       )}
 
       {open && (
-        <div className={column.main__open}>
+        <div className={column.main__open} >
           <div className={column.main} onClick={() => setOpen(false)}>
             <span>
               <i className={column.main__icon}></i>
@@ -57,15 +90,22 @@ function Column({ name, num, products }) {
 
           {!activeRow && (
             <>
-              <div className={column.products_list}>
+              <div className={column.products_list} ref={productListRef}>
                 {products.map((product, i) => (
                   <ProductOrder key={i} productData={product} />
                 ))}
               </div>
 
-              <div className={column.arrow_right}>
-                <i className={column.arrow_icon_right}></i>
-              </div>
+              {arrowRight && 
+                <div className={column.arrow_right}>
+                  <i className={column.arrow_icon_right}></i>
+                </div>
+              }
+              {arrowLeft && 
+                <div className={arrowLeft && arrowRight ? column.arrow_left + " " + column.arrow_up : column.arrow_left}>
+                  <i className={column.arrow_icon_left}></i>
+                </div>
+              }
             </>
           )}
         </div>

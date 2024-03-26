527ba116ffbe5c7e34fa93e5a804b18ae1bc3874
tzviki <twilhelm@votis.net>
2024-03-26 14:07:01 +0200

    Refactor ReturnFilterProduct component

--- frontend/src/components/manager/newOrder/ReturnFilterProduct.jsx
+++ frontend/src/components/manager/newOrder/ReturnFilterProduct.jsx
@@ -4,68 +4,63 @@ import filterProductStyle from "../../../assets/css/manager/newOrder/ReturnFilte
 import ProductReturn from "./ProductReturn";
 
 function ReturnFilterProduct() {
-    const { state, dispatch } = useMainContext();
-    const [products, setProducts] = useState([]);
-  
-    const filterProducts = () => {
-      let newProducts = [];
-      let allProducts = state.allProducts;
-      let displayFilters = state.displayFilters;
-  
-      for (let i = 0; i < allProducts.length; i++) {
-        if (
-          displayFilters["קבוצת משנה"] &&
-          displayFilters["קבוצת משנה"].includes(allProducts[i]["subGroupNumber"])
-        ) {
+  const { state, dispatch } = useMainContext();
+  const [products, setProducts] = useState([]);
+
+  const filterProducts = () => {
+    let newProducts = [];
+    let allProducts = state.allProducts;
+    let displayFilters = state.displayFilters;
+
+    for (let i = 0; i < allProducts.length; i++) {
+      if (
+        displayFilters["קבוצת משנה"] &&
+        displayFilters["קבוצת משנה"].includes(allProducts[i]["subGroupNumber"])
+      ) {
+        newProducts.push(allProducts[i]);
+      }
+      if (
+        displayFilters["ספק"] &&
+        displayFilters["ספק"].includes(allProducts[i]["providerNumber"])
+      ) {
+        if (!newProducts.includes(allProducts[i])) {
           newProducts.push(allProducts[i]);
         }
-        if (
-          displayFilters["ספק"] &&
-          displayFilters["ספק"].includes(allProducts[i]["providerNumber"])
-        ) {
-          if (!newProducts.includes(allProducts[i])) {
-            newProducts.push(allProducts[i]);
-          }
-        }
       }
-      setProducts(newProducts);
-    };
-  
-    const searchProducts = () => {
-      setProducts(state.searchResults);
-    };
-  
-    useEffect(() => {
-      if (state.displayFilters.length != 0) filterProducts();
-      if (state.search != '' ) searchProducts();
+    }
+    setProducts(newProducts);
+  };
 
-      if(state.search == '' && state.displayFilters.length == 0) {
-        setProducts([]);
-      }
-    }, [state.displayFilters, state.search]);
-  
-  
-    return (
-      <>
-      {products.length > 0 && 
+  const searchProducts = () => {
+    setProducts(state.searchResults);
+  };
+
+  useEffect(() => {
+    if (state.displayFilters.length != 0) filterProducts();
+    if (state.search != "") searchProducts();
+
+    if (state.search == "" && state.displayFilters.length == 0) {
+      setProducts([]);
+    }
+  }, [state.displayFilters, state.search]);
+
+  return (
+    <>
+      {products.length > 0 && (
         <div className={filterProductStyle.main}>
           {products.map((product) => (
-            <div  className={filterProductStyle.product}>
-                <ProductReturn key={product.barcode} productData={product} />
-            </div>
+            <ProductReturn key={product.barcode} productData={product} />
           ))}
         </div>
-      }
-      { (!state.displayFilters.length && !state.search) && !products.length  && 
+      )}
+      {!state.displayFilters.length && !state.search && !products.length && (
         <div className={filterProductStyle.main}>
-        {state.returnedProducts.map((product) => (
-          <div className={filterProductStyle.product}>
-              <ProductReturn  key={product.barcode} productData={product} />
-          </div>
-        ))}
-      </div>
-      }
-      </>
-    );
-  }
-export default ReturnFilterProduct
+          {state.returnedProducts.map((product) => (
+            <ProductReturn key={product.barcode} productData={product} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
+export default ReturnFilterProduct;

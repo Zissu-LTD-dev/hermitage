04f758498ccee4287c4327e73edcdd38f9b97e75
tzviki1304 <770tzv@gmail.com>
2025-01-06 09:38:19 +0200

    Increasing the submission of a patch request

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,9 @@ app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(cors())
 
+app.use(express.json({limit: '70mb'}));
+app.use(express.urlencoded({limit: '70mb', extended: true}));
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })

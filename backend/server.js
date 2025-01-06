5e93be98024d1a8c6117e32a5a976815f1e7ab01
tzviki1304 <770tzv@gmail.com>
2025-01-06 09:44:29 +0200

    .

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,11 @@ app.use(cookieParser())
 app.use(cors())
 
 app.use(express.json({limit: '70mb'}));
-app.use(express.urlencoded({limit: '70mb', extended: true}));
+app.use(express.urlencoded({
+    limit: '70mb',
+    extended: true,
+    parameterLimit: 100000
+}));
 
 app.get('/', (req, res) => {
     res.send('API is running...')
